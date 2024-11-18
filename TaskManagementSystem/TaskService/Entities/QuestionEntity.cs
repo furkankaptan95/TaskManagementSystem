2bebececee23e06ed34f0674a00b81988b10c85f
Furkan Kaptan <iamfurkan86@gmail.com>
2024-11-18 18:45:21 +0300

    TaskAPI QuestionEntity created.

---
+++ TaskManagementSystem/TaskService/Entities/QuestionEntity.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TaskAPI.Entities;
+public class QuestionEntity
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+    public string Content { get; set; }
+    public string? Answer { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? AnsweredAt { get; set; }
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string UserId { get; set; }
+    [BsonIgnore]
+    public UserEntity User { get; set; }
+}
