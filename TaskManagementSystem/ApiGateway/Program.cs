85ed4e9d26decd0235166a5b765f4b4dc0d5b35d
Furkan Kaptan <iamfurkan86@gmail.com>
2024-11-30 23:52:31 +0300

    ApiGateway - Other API's integration

--- TaskManagementSystem/ApiGateway/Program.cs
+++ TaskManagementSystem/ApiGateway/Program.cs
@@ -1,15 +1,20 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddJsonFile("ocelot.json",optional:false,reloadOnChange:true);
+// Add Ocelot middleware
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +22,11 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Add Ocelot Middleware
+app.UseOcelot().Wait();
 
 app.Run();
