b88e44e075fabded961f67afcaa7e75e726df9b8
Furkan Kaptan <iamfurkan86@gmail.com>
2025-01-26 15:30:29 +0300

    UserAPI jwt control mechanism added.

--- TaskManagementSystem/TaskService/Program.cs
+++ TaskManagementSystem/TaskService/Program.cs
@@ -1,6 +1,7 @@
 using TaskAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddServices(builder.Configuration);
 
 var app = builder.Build();
@@ -20,6 +21,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
