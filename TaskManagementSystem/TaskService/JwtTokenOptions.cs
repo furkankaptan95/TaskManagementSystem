1d4f4d6194f7bd2332fa5420d765b40fb5f83c1f
Furkan Kaptan <iamfurkan86@gmail.com>
2025-01-26 15:20:41 +0300

    TaskAPI jwt control mechanism added.

---
+++ TaskManagementSystem/TaskService/JwtTokenOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskAPI;
+public class JwtTokenOptions
+{
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+    public string Key { get; set; }
+    public string ExpireMinutes { get; set; }
+}
